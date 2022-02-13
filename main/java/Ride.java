1915dd250cb4f34aae79203493b2748e1aeda4d7
Priyaksm12 <priyankaks.priya@gmail.com>
2022-02-13 12:38:36 +0530

    JunitAssignment

---
+++ main/java/Ride.java
@@ -0,0 +1,18 @@
+public class Ride
+{
+
+    public double distance;
+    public double time;
+    public Ride(double distance, double time)
+    {
+        super();
+        this.distance = distance;
+        this.time = time;
+    }
+    @Override
+    public String toString()
+    {
+        return "Ride [distance=" + distance + ", time=" + time + "]";
+    }
+
+}
